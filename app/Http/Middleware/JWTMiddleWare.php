f00e4f0bdb5f9b50f2d683daaa90daea89b9be71
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-03-04 17:24:54 +0700

    first commit

---
+++ app/Http/Middleware/JWTMiddleWare.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+use JWTAuth;
+use Closure;
+use Illuminate\Http\Request;
+
+class JWTMiddleWare
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+    
+}
